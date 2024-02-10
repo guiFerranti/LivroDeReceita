686179cc7b631644fd0dc3cde288994b4c7434c8
guiFerranti <guilhermecousseau@icloud.com>
2024-02-10 17:48:31 -0300

    adicionando as classes de migration da tabela de usuario

---
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Migrations/Versions/BaseVersion.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using FluentMigrator.Expressions;
+using System.Reflection.Emit;
+
+namespace MeuLivroDeReceitas.Infrastructure.Migrations.Versions;
+
+public static class BaseVersion
+{
+    public static ICreateTableColumnOptionOrWithColumnSyntax InserirTabelaBase(ICreateTableWithColumnOrSchemaOrDescriptionSyntax tabela)
+    {
+        return tabela
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("DataCriacao").AsDateTime().NotNullable();
+    }
+}

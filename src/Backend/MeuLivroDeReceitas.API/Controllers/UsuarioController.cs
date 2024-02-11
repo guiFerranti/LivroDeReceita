c5c434badde6fd6e0d91db4d505239f2969253a6
guiFerranti <guilhermecousseau@icloud.com>
2024-02-11 19:36:40 -0300

    adicionando endpoint para registrar usuario

---
+++ src/Backend/MeuLivroDeReceitas.API/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using MeuLivroDeReceitas.Application.UseCases.Usuario.Registrar;
+using MeuLivroDeReceitas.Comunicacao.Request;
+using MeuLivroDeReceitas.Comunicacao.Response;
+using MeuLivroDeReceitas.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuLivroDeReceitas.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+
+    [HttpPost]
+    [ProducesResponseType(typeof(RespostaUsuarioRegistradoJson), StatusCodes.Status201Created)]
+    public async Task<IActionResult> RegistrarUsuario(
+        [FromServices] IRegistrarUsuarioUseCase useCase,
+        [FromBody] RequestRegistrarUsuarioJson user)
+    {
+        var result = await useCase.Executar(user);
+
+        return Created(string.Empty, result);
+    }
+
+
+}

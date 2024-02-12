456c3a4cf04506c39983d2d7d48ce37f852b0357
guiFerranti <guilhermecousseau@icloud.com>
2024-02-11 22:20:59 -0300

    criando testes de unidade para validator registrar usuario

---
+++ tests/Utilitario.ParaOsTestes/Requisicoes/RequisicaoRegistrarUsuarioBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using MeuLivroDeReceitas.Comunicacao.Request;
+
+namespace Utilitario.ParaOsTestes.Requisicoes;
+
+public class RequisicaoRegistrarUsuarioBuilder
+{
+    public static RequestRegistrarUsuarioJson Construir(int tamanhoSenha = 10)
+    {
+        return new Faker<RequestRegistrarUsuarioJson>()
+            .RuleFor(r => r.Nome, n => n.Person.FullName)
+            .RuleFor(r => r.Email, n => n.Internet.Email())
+            .RuleFor(r => r.Senha, n => n.Internet.Password(tamanhoSenha))
+            .RuleFor(r => r.Telefone, n => n.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{n.Random.Int(min: 1, max: 9)}"));
+    }
+}

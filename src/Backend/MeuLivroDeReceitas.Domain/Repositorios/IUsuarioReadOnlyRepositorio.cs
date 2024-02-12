4dbdc1cbb6898620225365fee842c0bc7fbffad8
guiFerranti <guilhermecousseau@icloud.com>
2024-02-12 15:22:59 -0300

    adicionando funcao no repositorio para fazer o login

--- src/Backend/MeuLivroDeReceitas.Domain/Repositorios/IUsuarioReadOnlyRepositorio.cs
+++ src/Backend/MeuLivroDeReceitas.Domain/Repositorios/IUsuarioReadOnlyRepositorio.cs
@@ -1,6 +1,9 @@
-﻿namespace MeuLivroDeReceitas.Domain.Repositorios;
+﻿using MeuLivroDeReceitas.Domain.Entidades;
+
+namespace MeuLivroDeReceitas.Domain.Repositorios;
 
 public interface IUsuarioReadOnlyRepositorio
 {
     Task<bool> ExisteUsuarioEmail(string email);
+    Task<Usuario> Login(string email, string senha);
 }

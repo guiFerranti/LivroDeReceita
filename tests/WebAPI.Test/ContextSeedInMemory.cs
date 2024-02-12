6855fa36ef7743775343410a474f8e5ba0da7501
guiFerranti <guilhermecousseau@icloud.com>
2024-02-12 19:29:45 -0300

    criando teste unitario para login na webapi

---
+++ tests/WebAPI.Test/ContextSeedInMemory.cs
@@ -0,0 +1,19 @@
+﻿using MeuLivroDeReceitas.Domain.Entidades;
+using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio;
+using Utilitario.ParaOsTestes.Entidades;
+
+namespace WebAPI.Test;
+
+public class ContextSeedInMemory
+{
+    public static (Usuario usuario, string senha) Seed(MeuLivroDeReceitasContext context)
+    {
+        (var usuario, string senha) = UsuarioBuilder.Construir();
+
+        context.Usuarios.Add(usuario);
+
+        context.SaveChanges();
+
+        return (usuario, senha);
+    }
+}
